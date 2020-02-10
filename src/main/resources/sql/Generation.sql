379fd0b318b935cf2bf40c9f771db9f8eb03f6c4
Nikolai023 <nn_gerasimenko@mail.ru>
2020-02-10 13:39:57 +0700

    Generation.sql added

---
+++ src/main/resources/sql/Generation.sql
@@ -0,0 +1,25 @@
+DROP TABLE numbers;
+
+CREATE TABLE numbers
+(
+    number INTEGER PRIMARY KEY
+);
+
+DO
+$$
+    DECLARE
+        i INTEGER := 0;
+    BEGIN
+        WHILE (i < 100)
+            LOOP
+                if (random() > 0.5) THEN
+                    INSERT INTO numbers (number) VALUES (i);
+                END IF;
+                i := i + 1;
+            END LOOP;
+    END;
+$$
+LANGUAGE plpgsql;
+
+SELECT *
+FROM numbers;
